da4e250d15c6065391ca7f8b82005f829b67e6cd
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2022-03-16 17:27:25 +0600

    implemented in td way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,12 +8,48 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
 
+  @ViewChild('f') signUpForm!: NgForm;
+  defaultQuestion: string = 'pet';
+  answer: string = '';
+  genders: string[] = ['male', 'female'];
+  hobbies: any = ['Gardening', 'Stamp collection', 'Paracycling']
+
+  user: any = {
+    name: '',
+    email: '',
+    gender: '',
+    secretQues: '',
+    answer: '',
+    hobby: ''
+  }
+
+  isSubmitted: boolean = false;
+
+
+  constructor(){
+    this.hobbies = this.hobbies.map((hobby: any) => ({name: hobby, selected: false}))
+  }
+
   suggestUsername(){
     const suggestedName = 'superstar';
+
+    this.signUpForm.form.patchValue({
+      userData: {
+        userName: suggestedName
+      }
+    })
   }
   
-  onSubmit(form: NgForm){
-    console.log('submitted');
-    console.log(form);
+
+  onSubmit(){
+    this.isSubmitted = true;
+    this.user.name = this.signUpForm.value.userData.userName;
+    this.user.email = this.signUpForm.value.userData.userEmail;
+    this.user.gender = this.signUpForm.value.gender;
+    this.user.secretQues = `name of your ${this.signUpForm.value.secret}`;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.hobby = this.signUpForm.value.hobby
+
+    this.signUpForm.reset();
   }
 }
